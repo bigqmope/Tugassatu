92ff834f8a0656a88e777c8dc40dfdef7dc172d1
bigqmope <adella.sweeta@gmail.com>
2025-08-28 13:01:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $makan = "Kuliner Pontianak";
             h1 { text-align: center;}
             body { font-family: verdana;
                  background-color: #FCEEED; }
-            footer { background-color: #FAE6E4; }
+            footer { background-color: #DC5648; }
             
         </style>
     </head>
